b60dc6cc298ba9f4a03fe3ca2ac41c86e36362f7
joeehab <youssef2101042@miuegypt.edu.eg>
2024-05-18 18:44:57 +0300

    trips dynamic

--- views/trips.php
+++ views/trips.php
@@ -1,4 +1,38 @@
-<!DOCTYPE html>
+<script>
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+</script>
+
+<?php
+require_once '../model/fetchModle.php';
+require_once '../controller/usercontroller.php';
+$usercontroller = new usercontroller();
+$fetchModle = new fetchModle();
+$conn = $usercontroller->getConn();
+if (!isset($_SESSION["login"]) || $_SESSION["login"] !== true) {
+    $result = mysqli_query($conn, " SELECT p.*, u.* FROM permissions p JOIN users u ON p.user_id = u.id WHERE p.guest = '1' ");
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION["login"] = true;
+    $_SESSION["id"] = $row["id"];
+} else if (!empty($_SESSION["id"])) {
+    $id = $_SESSION["id"];
+    $result = mysqli_query($conn, "SELECT a.*, p.*, u.* FROM addresses a JOIN permissions p ON a.user_id = p.user_id JOIN users u ON a.user_id = u.id WHERE a.user_id = '$id' AND u.id = '$id';");
+    $row = mysqli_fetch_assoc($result);
+} else {
+    header("Location: login");
+}
+
+// if (!empty($_SESSION['products']) && $row['guest'] == 1) {
+//   header("Location: login");
+// }
+
+if ($row["deactivated"] == 1) {
+    header("Location: deactivated");
+}
+
+include "header.php";
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,7 +47,6 @@
 
 <body>
     <header>
-        <?php include "header.php"; ?>
         <style>
             <?php include "../public/css/trips.css"; ?>
         </style>
@@ -74,42 +107,52 @@
             </button>
         </div>
     </div>
-    <div class="container products-carousel" style="margin-top: 5px;">
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-12">
-                    <div class="trip-card">
-                        <img src="../public/photos/hotelPhotos/hilton-alexandria-corniche.jpg" alt="Hotel" class="hotel-image">
-                        <div class="trip-details">
-                            <div class="trip-info">
-                                <h5>Best Western Plus</h5>
-                                <p class="label-available">Available - $1,234 per night</p>
-                                <p>Convenient for shopping - Too noisy</p>
-                            </div>
-                            <div class="flight-info">
-                                <h5><i class="bi bi-airplane-fill"></i> Flight Details</h5>
-                                <p><strong>Depart:</strong> 04:45 - CAI</p>
-                                <p><strong>Arrive:</strong> 17:00 - FRA</p>
-                                <p>1 stop - Via FCO</p>
-                                <span class="price">2,500 EGP</span>
-                                <button class="btn mitur position-absolute rounded-pill" style="margin-left: 120px; background-color: #0B2545; color: white; padding: 5px 10px;" id="book">
-                                    BOOK NOW
-                                </button>
+    <?php $sql = "SELECT t.ID AS trip_id , t.name , t.flightID , t.hotelID , f.dept_time, f.arr_time, f.flight_dep, f.flight_arr, f.eco_price, f.flight_day , h.name AS hotel_name, h.price, h.duration, h.photo
+        FROM trips t
+        JOIN flights f ON t.flightID = f.id
+        JOIN hotels h ON t.hotelID = h.ID";
+$result = mysqli_query($conn, $sql);
 
+if ($result->num_rows > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $totalprice = $row["price"] + $row["eco_price"];
+        echo "<div class='container products-carousel' style='margin-top: 5px;'>
+                <div class='container py-4'>
+                    <div class='row'>
+                        <div class='col-12'>
+                            <div class='trip-card'>
+                                <img src='" . $row["photo"] . "' alt='Hotel' class='hotel-image'>
+                                <div class='trip-details'>
+                                    <div class='trip-info'>
+                                    <h3>" . $row["name"] . " trip</h3>
+                                        <h5> At the " . $row["hotel_name"] . " hotel</h5>
+                                        <p class='label-available'>Available - " . number_format($row["price"],2) . " LE /night</p>
+                                        <p> Starting " . $row['flight_day'] .  " </p>
+                                    </div>
+                                    <div class='flight-info'>
+                                        <h5><i class='bi bi-airplane-fill'></i> Flight Details</h5>
+                                        <p><strong>Depart:</strong> " . $row["dept_time"] . " - " . $row["flight_dep"] . "</p>
+                                        <p><strong>Arrive:</strong> " . $row["arr_time"] . " - " . $row["flight_arr"] . "</p>
+                                        <p>" . $row["duration"]  . " days </p>
+                                        <span class='price'> " . number_format($totalprice,2) . " LE</span>
+                                        <button class='btn mitur position-absolute rounded-pill' style='margin-left: 120px; background-color: #0B2545; color: white; padding: 5px 10px;' id='book'>
+                                            BOOK NOW
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-
-
-    </div>
-
+            </div>";
+    }
+}
+?>
     <footer>
         <?php include "footer.php"; ?>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 <script>
     <?php include "../public/js/trips.js" ?>
